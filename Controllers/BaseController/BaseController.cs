eedc34821de601401afee2b29fd06597c1acb905
Reolight <95131769+Reolight@users.noreply.github.com>
2023-04-10 10:57:44 +0300

    Added BaseController to minimize controllers in the future

---
+++ Controllers/BaseController/BaseController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Claims;
+using DistantEdu.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualBasic;
+
+namespace DistantEdu.Controllers.BaseController
+{
+    // this template is to be future template for all controllers to reduce
+    // num of actions in each method.
+
+    [ApiController]
+    [Route("[controller]")]
+    public class BaseController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        protected ApplicationDbContext Context =>
+            _context ??= HttpContext.RequestServices.GetService<ApplicationDbContext>()!;
+
+        internal Guid UserId => !User.Identity.IsAuthenticated
+            ? Guid.Empty 
+            : Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier)!.Value);
+
+    }
+}
